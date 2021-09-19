5ee54f55d8d364b043d8c25fe24636e461de8cdf
Mohammed Marzouki <mmarzouk@e1r1p12.1337.ma>
2021-09-19 15:24:58 +0100

    working

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mmarzouk <mmarzouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:31:12 by mmarzouk          #+#    #+#             */
-/*   Updated: 2021/09/19 12:35:45 by mmarzouk         ###   ########.fr       */
+/*   Updated: 2021/09/19 15:24:35 by mmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ int main(int argc, char **argv)
 
 	if (!check_assign(argc, argv, &asset))
 		return (0);
-	
+	printf("working");
 }
